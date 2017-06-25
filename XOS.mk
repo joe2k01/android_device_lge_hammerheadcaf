20a42ea4fef373dc6d1c1ae886367580c6939c3a
Neil Agarwal <regalstreak@gmail.com>
2017-06-25 11:17:43 +0200

    hammerheadcaf: Move to new ota xml
    Change-Id: I7a24712b02787ff78410f5cd9b7acfae6b9fa443

--- XOS.mk
+++ XOS.mk
@@ -43,4 +43,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.ota.romname=$(PRODUCT_NAME) \
     persist.ota.version=$(shell date +%Y%m%d) \
-    persist.ota.manifest=https://raw.githubusercontent.com/halogenOS/android_extras_ota/XOS-7.1/$(PRODUCT_NAME).xml
+    persist.ota.manifest=https://raw.githubusercontent.com/halogenOS/android_extras_ota/XOS-7.1/hammerheadcaf.xml
